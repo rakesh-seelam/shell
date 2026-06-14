f1464796e43ededd5aaa9085ecf3c978e4fa1f16
rakesh <rakeshgautam602@gmail.com>
2026-06-14 10:01:33 +0530

    commiting conversation repo

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rama: Hello Hanuma, How are you today"
+echo "Hanuma: I am good Rama, I am good. What about you."
+echo "Rama: "I am great, Planning to learn shell scripting"
+echo "Hanuma: "Thats great. Let me know If can be of any assistance"
