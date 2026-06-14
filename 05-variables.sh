b5e61f805278eae67a46f124ef87d7bb54a9bd0c
rakesh <rakeshgautam602@gmail.com>
2026-06-14 10:21:43 +0530

    commiting 05-variables repo

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter your username"
+read "UNSER_NAME"
+
+echo "user name is $USER_NAME"
+
+echo "Please enter your password"
+read -s "PASSWORD"
+
+echo "password is $PASSWORD"
+
