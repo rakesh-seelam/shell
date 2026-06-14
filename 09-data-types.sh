522ec2db2907eb272d2f926138d3206d710af488
rakesh <rakeshgautam602@gmail.com>
2026-06-14 13:16:07 +0530

    commiting 09-data-types repo

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -7,3 +7,10 @@ SUM=$(($NUM1+$NUM2))
 
 echo "total is $SUM"
 
+cars=("Tata" "Kia" "Hyundai" "Maruthi")
+
+echo "cars are ${cars[@]}"
+echo "First car is ${cars[0]}"
+echo "Second car is ${cars[1]}"
+echo "Third car is ${cars[2]}"
+echo "Fourth car is ${cars[3]}"
