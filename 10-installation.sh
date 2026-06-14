cdd59223526346254440dcd1d64ceeed084420d8
rakesh <rakeshgautam602@gmail.com>
2026-06-14 13:39:11 +0530

    commiting 10-installation repo

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,8 +5,14 @@ USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
    echo "Run this script as root user"
    exit 1
-else
-   echo "Instatlling Nginx"
 fi 
 
+echo "Instatlling Nginx"
 dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+   echo "Installing Nginx is FAILURE"
+   exit 1
+else
+   echo "Installing Nginx is SUCCESS"
+fi
