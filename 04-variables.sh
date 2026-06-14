689778e34c3e1e22d253640334e78d370e3300d2
rakesh <rakeshgautam602@gmail.com>
2026-06-14 10:14:52 +0530

    commiting 04-variables repo

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, How are you today"
+echo "$PERSON2: I am good $PERSON1, What about you."
+echo "$PERSON1: I am great, Planning to learn shell scripting"
+echo "$PERSON2: Thats great $PERSON1. Let me know If can be of any assistance"
