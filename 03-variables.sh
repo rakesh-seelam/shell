91f0f9d3420735bac4be6cc8903ba94c23e39c95
rakesh <rakeshgautam602@gmail.com>
2026-06-14 10:12:12 +0530

    commiting variables repo

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ PERSON2=Hanuma
 echo "$PERSON1: Hello $PERSON2, How are you today"
 echo "$PERSON2: I am good $PERSON1, I am good. What about you."
 echo "$PERSON1: I am great, Planning to learn shell scripting"
-echo "$PERSON2: "Thats great. Let me know If can be of any assistance"
+echo "$PERSON2: Thats great. Let me know If can be of any assistance"
