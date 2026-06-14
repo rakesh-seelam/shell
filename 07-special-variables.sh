f938c3d5d7684c3d2649ab7277ccbe82498a5694
rakesh <rakeshgautam602@gmail.com>
2026-06-14 10:45:26 +0530

    modifying 07-special-variables repo

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -5,7 +5,7 @@ echo "number of variables passed to script: $#"
 echo "script name: $0"
 echo "present working directory: $PWD"
 echo "user running the script: $USER"
-echo "current home directory: $HOME
+echo "current home directory: $HOME"
 echo "process instance ID of current process: $$"
 sleep 10 &
 echo "background PID: $!"
