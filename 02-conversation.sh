26d85ce3521166588d657d4bc3615ba6d51665da
rakesh <rakeshgautam602@gmail.com>
2026-06-15 18:54:12 +0530

    commiting 13-logs repo

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,5 +2,5 @@
 
 echo "Rama: Hello Hanuma, How are you today"
 echo "Hanuma: I am good Rama, I am good. What about you."
-echo "Rama: "I am great, Planning to learn shell scripting"
+echo "Rama: I am great, Planning to learn shell scripting"
 echo "Hanuma: "Thats great. Let me know If can be of any assistance"
