55490bd69383cb6f642e911abddba74804bfdc82
rakesh <rakeshgautam602@gmail.com>
2026-06-20 12:15:35 +0530

    action

--- 16-backup.sh
+++ 16-backup.sh
@@ -49,7 +49,7 @@ log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIR"
 log "$DAYS"
 
-if [ z $FILES ]; then
+if [ z "$FILES" ]; then
    echo -e "No files to backup $Y SKIPPING $N "
 else
    echo -e "$G Files found for Archive $N"
