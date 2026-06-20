6a2a790ebfb5d098f403cc6d1bd2f009e7c96087
rakesh <rakeshgautam602@gmail.com>
2026-06-20 11:20:36 +0530

    action

--- 16-backup.sh
+++ 16-backup.sh
@@ -15,6 +15,12 @@ if [ $USERID -ne 0 ]; then
    echo -e "$R Run this script as root user $N"
    exit 1
 fi
+
+mkdir -p $LOGS_FOLDER
+
+log(){
+   echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
  
 USAGE(){
     echo -e "$R Usage: <SOURCE_DIR> <DEST_DIR> <DAYS>[Default 14] $N"
@@ -35,4 +41,15 @@ if [ ! -d "$DEST_DIR" ]; then
    exit 1
 fi
 
+#Checks if files are there in $SOURCE_DIR
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
+log "Destination Directory: $DEST_DIR"
+log "$DAYS"
+
+if [ z $FILES ]; then
+   echo -e "No files to backup $Y SKIPPING $N "
+fi
 
